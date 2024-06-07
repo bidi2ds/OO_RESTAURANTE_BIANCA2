85af154cbc04a0b911de0e75c3596332161d01df
LeticiaRompava <leticia.rompava@escola.pr.gov.br>
2024-06-07 12:26:04 -0300

    subindo restaurante3

--- restaurante2.py
+++ restaurante2.py
@@ -8,17 +8,5 @@ class Restaurante:
        # return self.nome
         return f'{self.nome}|{self.categoria}|{self.ativo}'
 
-restaurante_praca=Restaurante('Praça','italiana')
-# restaurante_praca.nome='Baeti'
-# restaurante_praca.categoria='Italiana'
 
-# restaurante_pasta=Restaurante()
-
-# restaurantes=[restaurante_praca,restaurante_pasta]
-
-#print(restaurante_praca.ativo)
-
-# print(dir(restaurante_praca))
-# print('')
-# print(vars(restaurante_praca))
 print(restaurante_praca)
