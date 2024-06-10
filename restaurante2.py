28411653e57906ed9915a07bf61a6047a6416957
bianca <biakosinski99@gmail.com>
2024-06-10 09:50:52 -0300

    subindo ativivdade3

--- restaurante2.py
+++ restaurante2.py
@@ -9,4 +9,17 @@ class Restaurante:
         return f'{self.nome}|{self.categoria}|{self.ativo}'
 
 
+restaurante_praca=Restaurante('Praça','Gourmet')
+# restaurante_praca.nome='Bistro'
+# restaurante_praca.categoria='Italiana'
+
+# restaurante_pizza=Restaurante()
+
+# restaurantes=[restaurante_praca,restaurante_pizza]
+
+#print(restaurante_praca.ativo)
+
+# print(dir(restaurante_praca))
+# print('')
+# print(vars(restaurante_praca))
 print(restaurante_praca)
